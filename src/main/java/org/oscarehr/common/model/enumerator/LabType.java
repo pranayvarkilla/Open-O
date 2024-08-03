113f09e221c11d83b6360a682412bc077915e4a2
Deval Italiya <deval.italiya@magentahealth.ca>
2024-08-03 15:24:43 +0000

    Minor changes

--- src/main/java/org/oscarehr/common/model/enumerator/LabType.java
+++ src/main/java/org/oscarehr/common/model/enumerator/LabType.java
@@ -5,31 +5,41 @@ package org.oscarehr.common.model.enumerator;
  * processing incoming lab reports.
  */
 public enum LabType {
-	ALPHA,
-	CML,
-	EPSILON,
-	ExcellerisON,
-	PATHL7,
-	EXCELLERIS,
-	GDML,
-	HHSEMR,
-	HRMXML,
-	ICL,
-	IHAPOI,
-	MDS,
-	MEDVUE,
-	HL7,
-	OLIS_HL7,
-	OSCAR_TO_OSCAR_HL7_V2,
-	PFHT,
-	SIOUX,
-	TDIS,
-	Spire,
-	OUL_R21,
-	ORU_R01,
-	BIOTEST,
-	CLS,
-	CDL,
-	TRUENORTH,
-	MEDITECH
+	ALPHA("ALPHA"),
+	CML("CML"),
+	EPSILON("EPSILON"),
+	ExcellerisON("ExcellerisON"),
+	PATHL7("PATHL7"),
+	EXCELLERIS("PATHL7"),
+	GDML("GDML"),
+	HHSEMR("HHSEMR"),
+	HRMXML("HRMXML"),
+	ICL("ICL"),
+	IHAPOI("IHA"),
+	MDS("MDS"),
+	MEDVUE("MEDVUE"),
+	HL7("HL7"),
+	OLIS_HL7("OLIS_HL7"),
+	OSCAR_TO_OSCAR_HL7_V2("OSCAR_TO_OSCAR_HL7_V2"),
+	PFHT("PFHT"),
+	SIOUX("SIOUX"),
+	TDIS("TDIS"),
+	Spire("Spire"),
+	OUL_R21("OUL_R21"),
+	ORU_R01("ORU_R01"),
+	BIOTEST("BIOTEST"),
+	CLS("CLS"),
+	CDL("CDL"),
+	TRUENORTH("TRUENORTH"),
+	MEDITECH("MEDITECH");
+
+	private final String name;
+
+	LabType(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
 }
