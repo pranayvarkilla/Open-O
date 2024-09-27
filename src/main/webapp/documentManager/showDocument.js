166e51e13e3666bea24d91de961bb182a207ac5c
Chitrank <cdave@openosp.ca>
2024-09-27 10:28:24 -0400

    Fix demographic search to include inactive patients for document linking in Inbox
    The issue occurred when unchecking the "Active Only" checkbox. Changes include:
    
    - Moved the `setupDemoAutoCompletion` function into showDocument.js file to make it accessible globally.
       The function was in the showDocument.jsp scope and was not accessible to the parent Page.jsp in Preview-mode.
    - Updated the function to handle the `Active Only` checkbox state dynamically, ensuring that both active and inactive patients can be searched.

--- src/main/webapp/documentManager/showDocument.js
+++ src/main/webapp/documentManager/showDocument.js
@@ -19,3 +19,43 @@ function popupPatientTickler(height, width, url, windowName,docId) {
 	  urlNew = url + "demographic_no="+d+"&name="+n+"&chart_no=&bFirstDisp=false&messageID=null&remoteFacilityId=&docType=DOC&docId="+docId;
 	  return popup2(height, width, 0, 0, urlNew, windowName);
 }
+
+function setupDemoAutoCompletion(docId, contextPath) {
+	let autoCompleteId = "#autocompletedemo" + docId;
+	if (jQuery(autoCompleteId)) {
+
+		var url = contextPath + "/demographic/SearchDemographic.do?jqueryJSON=true&activeOnly=" + jQuery("#activeOnly" + docId).is(":checked");
+
+		jQuery(autoCompleteId).autocomplete({
+			source: url, minLength: 2,
+
+			focus: function (event, ui) {
+				jQuery(autoCompleteId).val(ui.item.label);
+				return false;
+			}, select: function (event, ui) {
+				jQuery(autoCompleteId).val(ui.item.label);
+				jQuery("#demofind" + docId).val(ui.item.value);
+				jQuery("#demofindName" + docId).val(ui.item.formattedName);
+				selectedDemos.push(ui.item.label);
+				console.log(ui.item.providerNo);
+
+				if (ui.item.providerNo !== undefined && ui.item.providerNo != null && ui.item.providerNo !== "" && ui.item.providerNo !== "null") {
+					addDocToList(ui.item.providerNo, ui.item.provider + " (MRP)", docId);
+				}
+
+				// enable Save button whenever a selection is made
+				jQuery('#save' + docId).removeAttr('disabled');
+				jQuery('#saveNext' + docId).removeAttr('disabled');
+
+				jQuery('#msgBtn_' + docId).removeAttr('disabled');
+				jQuery('#mainTickler_' + docId).removeAttr('disabled');
+				jQuery('#mainEchart_' + docId).removeAttr('disabled');
+				jQuery('#mainMaster_' + docId).removeAttr('disabled');
+				jQuery('#mainApptHistory_' + docId).removeAttr('disabled');
+
+				return false;
+			}
+		});
+	}
+}
+
