d98a5053077318013fe29b5252596f3c9eb3c5a4
gongjian1981 <gongjian1981@gmail.com>
2025-01-24 10:26:41 -0500

    fix: fixed the compilation errors (25 errors left)
     - fix setAllowMTOM() doesn't exist error by adding config to JaxWsProxyFactoryBean

--- src/main/java/org/oscarehr/integration/ebs/client/ng/EdtClientBuilder.java
+++ src/main/java/org/oscarehr/integration/ebs/client/ng/EdtClientBuilder.java
@@ -14,6 +14,7 @@ import jakarta.xml.soap.SOAPFactory;
 import jakarta.xml.ws.BindingProvider;
 import jakarta.xml.ws.soap.SOAPBinding;
 
+import org.apache.cxf.binding.soap.SoapBindingConfiguration;
 import org.apache.cxf.configuration.jsse.TLSClientParameters;
 import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.frontend.ClientProxy;
@@ -209,6 +210,9 @@ public class EdtClientBuilder {
 		JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
 		factory.setServiceClass(clientClass);
 		factory.setAddress(getConfig().getServiceUrl());
+		SoapBindingConfiguration config = new SoapBindingConfiguration();
+		config.setMtomEnabled(true);
+		factory.setBindingConfig(config);
 		return (T) factory.create();
 	}
 
@@ -287,7 +291,7 @@ public class EdtClientBuilder {
 
 		Map<String, Object> outProps = newWSSOutInterceptorConfiguration();
 		WSS4JOutInterceptor wssOut = new WSS4JOutInterceptor(outProps);
-		wssOut.setAllowMTOM(true);
+		//wssOut.setAllowMTOM(true);
 		client.getEndpoint().getOutInterceptors().add(wssOut);
 	}
 
